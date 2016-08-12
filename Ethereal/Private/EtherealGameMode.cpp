d0fdf5bbc2f02b348a728aab1953f00f81193d32
Scott McCutchen <scott.mccutchen@soverance.com>
2016-08-12 17:15:27 -0400

    Set Default Classes
    Set the Default Classes via Game Mode. Created a Player Controller BP.

--- Ethereal/Private/EtherealGameMode.cpp
+++ Ethereal/Private/EtherealGameMode.cpp
@@ -6,5 +6,23 @@
 AEtherealGameMode::AEtherealGameMode(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	
+	// Set Default Classes
+
+	//GameInstanceClass = AEtherealGameInstance::StaticClass();
+
+	//ADefaultPawn = AEtherealGameMode::StaticClass();
+
+	PlayerControllerClass = AEtherealPlayerController::StaticClass();
+
+	PlayerStateClass = AEtherealPlayerState::StaticClass();
+
+	GameStateClass = AEtherealGameState::StaticClass();
+
+}
+
+void AEtherealGameMode::PlayerLogin(AEtherealPlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	NewPlayer->ClientPostLogin();
 }
--- Ethereal/Public/EtherealGameMode.h
+++ Ethereal/Public/EtherealGameMode.h
@@ -1,15 +1,25 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "GameFramework/GameMode.h"
+//#include "EtherealGameInstance.h"
+#include "EtherealPlayerController.h"
+#include "EtherealPlayerState.h"
+#include "EtherealGameState.h"
 #include "EtherealGameMode.generated.h"
 
-UCLASS(minimalapi)
+UCLASS()
 class AEtherealGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
 public:
 	AEtherealGameMode(const FObjectInitializer& ObjectInitializer);
+
+	/** Called immediately after the game begins. */
+	UFUNCTION(BlueprintCallable, Category = Default)
+	void PlayerLogin(AEtherealPlayerController* NewPlayer);
+
+	
 };
 
 
