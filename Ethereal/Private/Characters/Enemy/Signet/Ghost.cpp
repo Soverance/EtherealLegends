a10bd2456c36229ab11901072103370e232456c1
Scott McCutchen <scott.mccutchen@soverance.com>
2017-03-23 15:24:57 -0400

    GetAdjustedLevel() changes
    These changes were released with version 1.2.

--- Ethereal/Private/Characters/Enemy/Signet/Ghost.cpp
+++ Ethereal/Private/Characters/Enemy/Signet/Ghost.cpp
@@ -32,8 +32,8 @@ AGhost::AGhost(const FObjectInitializer& ObjectInitializer)
 	Realm = ERealms::R_Arcadia;
 	BattleType = EBattleTypes::BT_Signet;
 	CommonDrop = EMasterGearList::GL_PulseArrow;
-	UncommonDrop = EMasterGearList::GL_HuntersBoots;
-	RareDrop = EMasterGearList::GL_CrimsonGreaves;
+	UncommonDrop = EMasterGearList::GL_HuntersPants;
+	RareDrop = EMasterGearList::GL_CrimsonSlacks;
 	AttackDelay = 2.0f;
 	BaseEyeHeight = 16;
 	GetCapsuleComponent()->SetRelativeScale3D(FVector(1.0f, 1.0f, 1.0f));
