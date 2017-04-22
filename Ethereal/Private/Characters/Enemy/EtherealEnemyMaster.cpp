bb5c63afddadf7580335ece8181ae5dc7559a915
Scott McCutchen <scott.mccutchen@soverance.com>
2017-04-21 21:18:04 -0400

    Enemy Base Stat Variations
    see Enemy Master for stat variations

--- Ethereal/Private/Characters/Enemy/EtherealEnemyMaster.cpp
+++ Ethereal/Private/Characters/Enemy/EtherealEnemyMaster.cpp
@@ -123,8 +123,8 @@ void AEtherealEnemyMaster::Tick(float DeltaTime)
 // Sets the Enemy's default stats
 void AEtherealEnemyMaster::SetBaseStats()
 {
-	float HPMod = 0;
-	float StatMod = 0.0f;
+	float HPMod = 0;  
+	float StatMod = 0.0f; 
 
 	if (BattleType == EBattleTypes::BT_Signet) // High stats for SNMs
 	{
@@ -142,11 +142,13 @@ void AEtherealEnemyMaster::SetBaseStats()
 		StatMod = 5.1f;	
 	}
 
+	// SET ALL STATS BASED ON CURRENT LEVEL + MODIFIERS
 	HP_Current = Level * HPMod;
 	HP_Max = Level * HPMod;
-	ATK = Level * StatMod;
-	DEF = Level * StatMod;
-	SPD = Level * StatMod;
+	// the randoms are just to add slight variations to the enemy's stats.
+	ATK = (Level * StatMod) + FMath::RandRange(-3.0f, 3.0f);
+	DEF = (Level * StatMod) + FMath::RandRange(-3.0f, 3.0f);
+	SPD = (Level * StatMod) + FMath::RandRange(-3.0f, 3.0f);
 }
 
 // Force HP Caps keeps the enemy's HP between 0 and Max
