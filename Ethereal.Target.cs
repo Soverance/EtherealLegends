71c499bde3ff343f1bf8118ceda62acce162d35d
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-04-02 23:53:28 -0400

    Updated for UE 4.19

--- Ethereal.Target.cs
+++ Ethereal.Target.cs
@@ -1,4 +1,4 @@
-// © 2014 - 2017 Soverance Studios
+// Â© 2014 - 2017 Soverance Studios
 // http://www.soverance.com
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,7 +22,7 @@ public class EtherealTarget : TargetRules
         : base(Target)
     {
         Type = TargetType.Game;
-        ExtraModuleNames.Add("Ethereal");
+        ExtraModuleNames.AddRange(new string[] { "Ethereal" });
         bUsesSteam = true;
     }
 
--- EtherealEditor.Target.cs
+++ EtherealEditor.Target.cs
@@ -1,4 +1,4 @@
-// © 2014 - 2017 Soverance Studios
+// Â© 2014 - 2017 Soverance Studios
 // http://www.soverance.com
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,6 +23,6 @@ public class EtherealEditorTarget : TargetRules
         : base(Target)
     {
 		Type = TargetType.Editor;
-        ExtraModuleNames.Add("Ethereal");
+        ExtraModuleNames.AddRange(new string[] { "Ethereal" });
     }
 }
